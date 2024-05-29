af3893bf920100b6bc42736b378f1128a428e6c0
akusayadaffa <daffahadian8@gmail.com>
2024-05-29 18:18:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,26 +13,31 @@ var lowerHue = document.getElementById('lower-hue');
 var lowerSaturation = document.getElementById('lower-saturation');
 var lowerLightness = document.getElementById('lower-lightness');
 
-// Add event listeners for both click and touch events
-explode.addEventListener('click', explodeMinifigure);
-explode.addEventListener('touchstart', explodeMinifigure);
-randomize.addEventListener('click', randomizeInputs);
-randomize.addEventListener('touchstart', randomizeInputs);
-expressionRangeInput.addEventListener('input', setExpression);
-expressionRangeInput.addEventListener('touchstart', setExpression);
+function addEventListeners() {
+  explode.addEventListener('click', explodeMinifigure);
+  explode.addEventListener('touchstart', explodeMinifigure);
+  randomize.addEventListener('click', randomizeInputs);
+  randomize.addEventListener('touchstart', randomizeInputs);
+  expressionRangeInput.addEventListener('input', setExpression);
+  expressionRangeInput.addEventListener('touchstart', setExpression);
 
-for (var i = 0; i < colorRangeInput.length; i++) {
-  colorRangeInput[i].addEventListener('input', setColors);
-  colorRangeInput[i].addEventListener('touchstart', setColors);
+  for (var i = 0; i < colorRangeInput.length; i++) {
+    colorRangeInput[i].addEventListener('input', setColors);
+    colorRangeInput[i].addEventListener('touchstart', setColors);
+  }
 }
+
+addEventListeners();
 
 function getRandomNum(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-function explodeMinifigure() {
+function explodeMinifigure(event) {
+  event.preventDefault(); // Prevent default behavior
+  console.log('explodeMinifigure called');
   minifigure.classList.toggle('explode');
-  
+
   if (minifigure.classList.contains('explode')) {
     explode.innerHTML = 'Rebuild';
   } else {
@@ -40,7 +45,9 @@ function explodeMinifigure() {
   }
 }
 
-function randomizeInputs() {
+function randomizeInputs(event) {
+  event.preventDefault(); // Prevent default behavior
+  console.log('randomizeInputs called');
   var randomExpression = getRandomNum(0, 5);
   var randomUpperHue = getRandomNum(0, 360);
   var randomUpperSaturation = getRandomNum(0, 100);
@@ -48,7 +55,7 @@ function randomizeInputs() {
   var randomLowerHue = getRandomNum(0, 360);
   var randomLowerSaturation = getRandomNum(0, 100);
   var randomLowerLightness = getRandomNum(0, 90);
-  
+
   expressionRangeInput.value = randomExpression * 100;
   upperHue.value = randomUpperHue;
   upperSaturation.value = randomUpperSaturation;
@@ -56,24 +63,26 @@ function randomizeInputs() {
   lowerHue.value = randomLowerHue;
   lowerSaturation.value = randomLowerSaturation;
   lowerLightness.value = randomLowerLightness;
-  
+
   setExpression();
   setColors();
 }
 
-function setExpression() {
+function setExpression(event) {
+  console.log('setExpression called');
   var expressionVal = parseInt(expressionRangeInput.value);
   faces.style.transform = 'translateX(-' + expressionVal + '%)';
 }
 
-function setColors() {
+function setColors(event) {
+  console.log('setColors called');
   var upperHueVal = parseInt(upperHue.value);
   var upperSaturationVal = parseInt(upperSaturation.value);
   var upperLightnessVal = parseInt(upperLightness.value);
   var lowerHueVal = parseInt(lowerHue.value);
   var lowerSaturationVal = parseInt(lowerSaturation.value);
   var lowerLightnessVal = parseInt(lowerLightness.value);
-  
+
   upperBody.style.color = 'hsl(' + upperHueVal + ',' + upperSaturationVal + '%,' + upperLightnessVal + '%)';
   lowerBody.style.color = 'hsl(' + lowerHueVal + ',' + lowerSaturationVal + '%,' + lowerLightnessVal + '%)';
 }
